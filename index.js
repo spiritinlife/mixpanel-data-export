8cfd9a031c12cc0e396a8fd3f7dffba3bf24aef1
George Chailazopoulos <george.chailazopoulos@gmail.com>
2016-05-25 18:53:46 +0300

    Add basic authentication and quering in mixpanel

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+  request = require('request'),
+  moment = require('moment'),
+  app = express();
 
 if (!process.env.MIXPANEL_SECRET) {
   console.log('No app secret defined. Please define the "MIXPANEL_EVENT" environment variable!');
   return;
 }
-var baseUrl = "https://" + process.env.MIXPANEL_SECRET + "@mixpanel.com/api/2.0";
+var basicHeader = ' Basic ' + new Buffer(process.env.MIXPANEL_SECRET).toString('base64')
+var baseUrl = "https://mixpanel.com/api/2.0";
 
 
 app.get('/reports/:venue_id', function (req, res) {
-  var venue = req.params.venue_id
+  var venue = req.params.venue_id;
+  var today = moment();
+  var queryString = '?';
 
-  return res.status(200);
+  queryString += 'event=' + 'View venue card';
+  queryString += '&from_date=' + today.format('YYYY-MM-DD');
+  queryString += '&to_date=' + today.format('YYYY-MM-DD');
+  queryString += '&interval=1';
+
+  request({
+      url: baseUrl + '/segmentation' + queryString,
+      headers: {'Authorization': basicHeader}
+    }, function (error, response, body) {
+      console.log(error, body);
+      if (!error && response.statusCode == 200) {
+        return res.status(200).json(body);
+      } else {
+        return res.status(400).json(error);
+      }
+    }
+  )
+
 });
 
 app.listen(3000, function () {
