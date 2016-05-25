c23f405334cd200b289afe4815b47722f225c6a0
George Chailazopoulos <george.chailazopoulos@gmail.com>
2016-05-25 18:16:04 +0300

    Construct base url, assert on no mixpanel secret

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-//reports/:id
-//segmantation eendpoint
-
-
 var express = require('express');
 var app = express();
 
+if (!process.env.MIXPANEL_SECRET) {
+  console.log('No app secret defined. Please define the "MIXPANEL_EVENT" environment variable!');
+  return;
+}
+var baseUrl = "https://" + process.env.MIXPANEL_SECRET + "@mixpanel.com/api/2.0";
+
 
 app.get('/reports/:venue_id', function (req, res) {
-  res.send('the venue_id is' + req.params.venue_id);
+  var venue = req.params.venue_id
+
+  return res.status(200);
 });
 
 app.listen(3000, function () {
